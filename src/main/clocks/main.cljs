72757cbb25b44eb480e4e53ded11625d9124bfa4
alonso.torres <alonso.torres@kaleidos.net>
2021-11-12 13:13:02 +0100

    Exercise 1 - done

--- src/main/clocks/main.cljs
+++ src/main/clocks/main.cljs
@@ -17,10 +17,13 @@
   [event]
   ;; Get text node
   ;; Change text content
+  (let [node (.getElementById js/document "text")]
+    (aset node "textContent" "Something Nice!"))
   )
 
 (defn setup
   []
   ;; Get button node #click-btn
   ;; Add "click" event listener
-  )
+  (let [btn (.getElementById js/document "click-btn")]
+    (.addEventListener btn "click" handle-click)))
