a12bf78a3c5ae92f4bfe893ed68ed7dfc2dbce2c
alonso.torres <alonso.torres@kaleidos.net>
2021-11-12 13:26:04 +0100

    Exercise 2 - done

--- src/main/clocks/main.cljs
+++ src/main/clocks/main.cljs
@@ -20,21 +20,27 @@
   []
   ;; Get the #counter node
   ;; Change textContent to reflect the current counter value
-  )
+  (let [node (.getElementById js/document "counter")]
+    (aset node "textContent" (str @counter))))
 
 (defn add-counter
   [event]
   ;; Add to counter
   ;; Refresh text
-  )
+  (swap! counter (fn [old-value] (+ old-value 1)))
+  (refresh-text))
 
 (defn sub-counter
   [event]
   ;; Substract from counter
   ;; Refresh text
-  )
+  (swap! counter (fn [old-value] (- old-value 1)))
+  (refresh-text))
 
 (defn setup []
   ;; Get button nodes #add-btn #sub-btn
   ;; Add event listeners
-  )
+  (let [add-btn (.getElementById js/document "add-btn")
+        sub-btn (.getElementById js/document "sub-btn")]
+    (.addEventListener add-btn "click" add-counter)
+    (.addEventListener sub-btn "click" sub-counter)))
