0fdbd691e3d8f1ac50619e35d7e744186995c099
alonso.torres <alonso.torres@kaleidos.net>
2021-11-15 12:30:36 +0100

    Exercise 4 - Done

--- src/main/clocks/main.cljs
+++ src/main/clocks/main.cljs
@@ -14,13 +14,43 @@
                   :height nil}))
 
 (defn setup
-  [])
+  []
+  (let [canvas (.getElementById js/document "canvas")
+        rect (.getBoundingClientRect canvas)]
+    (reset! state
+            {:width (.-width rect)
+             :height (.-height rect)
+             :context (.getContext canvas "2d")})
+
+    (aset canvas "width" (:width @state))
+    (aset canvas "height" (:height @state))))
 
 (defn random-color
-  [])
+  []
+  (let [r (* (.random js/Math) 255)
+        g (* (.random js/Math) 255)
+        b (* (.random js/Math) 255)]
+    {:r r :g g :b b}))
 
 (defn draw
-  [])
+  []
+  (let [context (:context @state)
+        width   (:width @state)
+        height  (:height @state)
+
+        color (random-color)]
+
+    (.clearRect context 0 0 width height)
+
+    (.beginPath context)
+    (aset context "lineWidth" 5)
+    (.rect context 10 10 (- width 20) (- height 20))
+    (.stroke context)
+
+    (.beginPath context)
+    (aset context "fillStyle" (str "rgb(" (:r color) "," (:g color) "," (:b color) ")"))
+    (.rect context 10 10 (- width 20) (- height 20))
+    (.fill context)))
 
 (defn start-loop
   []
