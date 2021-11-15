33239f87e0df34c7e8f7e74b375fc12fd894c64a
alonso.torres <alonso.torres@kaleidos.net>
2021-11-15 11:28:21 +0100

    Exercise 3 - done

--- src/main/clocks/main.cljs
+++ src/main/clocks/main.cljs
@@ -19,14 +19,34 @@
 (defn setup
   []
   ;; Setup canvas
-
   ;; Store in the state: context, width and height
-  )
+
+  (let [canvas (.getElementById js/document "canvas")
+        rect (.getBoundingClientRect canvas)]
+
+    (reset! context (.getContext canvas "2d"))
+    (reset! width (.-width rect))
+    (reset! height (.-height rect))
+
+    (aset canvas "width" @width)
+    (aset canvas "height" @height)))
 
 (defn draw
   []
   ;; Draw lines
-  )
+  (.clearRect @context 0 0 width height)
+  (.beginPath @context)
+  (aset "lineWidth" @context 5)
+
+  (doseq [l hlines]
+    (.moveTo @context 0 (* @height l))
+    (.lineTo @context @width (* @height l)))
+
+  (doseq [l vlines]
+    (.moveTo @context (* @width l) 0)
+    (.lineTo @context (* @width l) @height))
+
+  (.stroke @context))
 
 (defn init []
   (setup)
